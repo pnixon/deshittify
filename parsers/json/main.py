0d7430882d096b31889acd1e3808bc067ef1b05c
Pete <nixon.pete@gmail.com>
2025-03-02 16:11:44 -0800

    removing unnecessary python code

--- parsers/json/main.py
+++
@@ -1,5 +0,0 @@
-import json
-
-def parse(input: str):
-    with open(input, 'r') as file:
-        return json.load(file)
--- server.py
+++
@@ -1,6 +0,0 @@
-import parsers.json.main as json_parser
-from parsers.args.arg_parser import ArgParser
-
-arg_parser = ArgParser()
-
-print(json_parser.parse(arg_parser.args.file))
